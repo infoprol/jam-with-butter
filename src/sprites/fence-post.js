77daa33678e83a3efeeb35985d17a05dfbf6de7b
Stephen Riley <stephenriley@themediaowl.com>
2017-04-22 18:31:08 -0500

    adding fence post sprite

--- src/sprites/fence-post.js
+++ src/sprites/fence-post.js
@@ -1,8 +1,10 @@
-import Phaser from 'phaser';
+import Phaser from 'phaser'
 
 export default class FencePost extends Phaser.Sprite {
     constructor (game, x, y, asset) {
-    	super(game, x, y, asset);
+    	super(game, x, y, asset)
+
+    	this.game.add.existing(this);
 
         // this.x
         // this.y
--- src/sprites/fence.js
+++ src/sprites/fence.js
@@ -1,13 +1,15 @@
-import Phaser from 'phaser';
-import FencePost from '../sprites/fence-post';
+import Phaser from 'phaser'
+import FencePost from '../sprites/fence-post'
 
 export default class Fence {
     constructor (game, x, y, asset, numPosts) {
 
-        this.posts = new Set();
+        this.posts = new Set()
 
         for (let i = 0; i < numPosts; i++) {
-        	this.posts.add(new FencePost(game, x, y, asset, numPosts));
+
+        	// change to x/y position of actual post position
+        	this.posts.add(new FencePost(game, x+(i*10), y+(i*10), 'fencePost', numPosts))
         }
     }
 
