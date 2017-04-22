670a9edbd5e6a8b49a82b4f28438e1c412527d53
Matthew Traylor <mhtraylor@hotmail.com>
2017-04-22 18:01:48 -0500

    fixed use of weakmap in controller

--- src/core/controller.js
+++ src/core/controller.js
@@ -1,16 +1,16 @@
 import 'phaser'
 
 export default function (params, game) {
-  const keys = new WeakMap()
-  const events = new WeakMap()
+  const keys = []
+  const events = {}
   let ctrl = {}
   for (const k of params) {
     let key = Object.assign(k, {phaserKey: game.input.keyboard.addKey(k.key)})
-    keys.set(k.name, key)
+    keys.push(key)
 
     let [u, d] = [`${k.name}UP`, `${k.name}DOWN`]
-    events.set(u, new Event(u))
-    events.set(d, new Event(d))
+    events[u] = new Event(u)
+    events[d] = new Event(d)
 
     ctrl[k.name] = (p, r) => {
       document.addEventListener(d, p)
@@ -20,9 +20,9 @@ export default function (params, game) {
     ctrl.update = () => {
       for (const k of keys) {
         if (k.phaserKey.isDown) {
-          document.dispatchEvent(events.get(d))
+          document.dispatchEvent(events[d])
         } else if (k.phaserKey.isUp) {
-          document.dispatchEvent(events.get(u))
+          document.dispatchEvent(events[u])
         }
       }
     }
