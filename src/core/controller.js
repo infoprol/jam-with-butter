d5bb74029ca5719f0daa48a3eedba62ab11151a8
Matthew Traylor <mhtraylor@hotmail.com>
2017-04-22 18:35:28 -0500

    controller now passes key so that up state can be checked

--- src/core/controller.js
+++ src/core/controller.js
@@ -1,17 +1,28 @@
 import 'phaser'
+
 export default function (params, game) {
-  const keys   = []
-  const events = new WeakMap()
-  let ctrl     = {}
+  const keys = []
+  const events = new Map()
+  let ctrl = {}
   for (const k of params) {
-    events.set(k, new Event(k.name))
-    ctrl[k.name] = (f) => document.addEventListener(k.name, f)
-    keys.push(Object.assign(k, {phaserKey: game.input.keyboard.addKey(k.key)}))
+    let key = Object.assign(k, {phaserKey: game.input.keyboard.addKey(k.key)})
+    keys.push(key)
+
+    events.set(key, new CustomEvent(key.name, {
+      detail: {
+        key: key.phaserKey
+      }
+    }))
+
+    ctrl[k.name] = (f) => document.addEventListener(key.name, f)
   }
+
   return Object.assign(ctrl, {
     update: () => {
       for (const k of keys) {
+        console.log(k.phaserKey.isDown)
         if (k.phaserKey.isDown) {
+          console.log('dispathcin event')
           document.dispatchEvent(events.get(k))
         }
       }
