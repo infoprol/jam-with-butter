2eccd9ea243066ba037f856313aaa67c196a4d0f
Stephen Riley <stephenriley@themediaowl.com>
2017-04-22 23:48:07 -0500

    fixing fence post anchor; removing logs

--- src/sprites/fence-post.js
+++ src/sprites/fence-post.js
@@ -2,14 +2,10 @@ import Phaser from 'phaser'
 
 export default class FencePost extends Phaser.Sprite {
     constructor (game, x, y, asset) {
-
-    	console.log('x:'+x )
-    	console.log('y:'+y )
-    	console.log('----' )
-
     	super(game, x, y, asset)
 
-    	this.game.middleLayer.add(this);
+    	this.anchor.setTo(0.5)
+    	this.game.middleLayer.add(this)
 
         // this.x
         // this.y
--- src/sprites/fence.js
+++ src/sprites/fence.js
@@ -6,33 +6,22 @@ import {partitionPoints} from '../maths-box'
 export default class Fence {
     constructor (game, {x,y}, phase, asset, numPosts, radius) {
         
+        this.posts = new Set()
 
         const [xf,yf] = [
             x + (radius * Math.cos(phase)),
             y + (radius * Math.sin(phase))
-        ];
-        
-        
-
-
+        ]
         
         const pp = partitionPoints(
             [x,y],
             [xf,yf],
             numPosts
-        );
-
-        console.log('PP----');
-        console.log(pp);
-        console.log('End PP----');
+        )
         
-        this.posts = new Set()
+        
         for (let i = 0; i < pp.length; i++) {
             let {x,y} = pp[i]
-
-            console.log('create x/y: ' + x + '/' + y);
-
-        	// change to x/y position of actual post position
         	this.posts.add(new FencePost(game, x, y, 'fencePost', numPosts))
         }
         
